33733fc809618f477d10ab4611e1df018c8ab948
PragathiYerra <ysspragathi@gmail.com>
2022-09-10 21:35:59 +0530

    stashed

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // this is javascript file
 var deekshi = "sony"
 console.log(deekshi)
+
+console.log(deekshi)
+console.log(deekshi)
+
+console.log(sony)
+console.log(chitti)
+
+console.log("suchi")
